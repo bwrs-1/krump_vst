fe890006dd7638b69b743c542977d7137b635245
bwrs-1 <73294528+bwrs-1@users.noreply.github.com>
2025-05-16 17:16:09 +0900

    Update plugin with improvements

--- src/PluginProcessor.h
+++ src/PluginProcessor.h
@@ -3,6 +3,8 @@
 #include <juce_audio_processors/juce_audio_processors.h>
 #include <juce_dsp/juce_dsp.h>
 
+class AudioPluginAudioProcessorEditor;
+
 class AudioPluginAudioProcessor final : public juce::AudioProcessor
 {
 public:
